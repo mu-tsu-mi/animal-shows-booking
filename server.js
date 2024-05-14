8f9c18f5b504ce183a677e069f9245c9567072bf
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2024-05-14 21:32:37 +1000

    add db connection

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const methodOverride = require('method-override')
+require('dotenv').config()
+require('./db/database')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
