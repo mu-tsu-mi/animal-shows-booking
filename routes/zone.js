48d3a2e8531dcd7c9315438689c43593beefb3df
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2024-05-15 12:22:45 +1000

    add zone ejs

--- routes/zone.js
+++ routes/zone.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Animal\'s Wonderland' });
+  res.render('zone');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 require('./db/database')
 
 var indexRouter = require('./routes/index');
+const zoneRouter = require('./routes/zone')
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
+app.use('/zone', zoneRouter)
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
