c43d027ae95dabde43725d67420db3c0bbfa1c1c
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2024-05-17 11:49:17 +1000

    change animal zone name

--- controllers/animal-zone.js
+++ controllers/animal-zone.js
@@ -7,17 +7,17 @@ module.exports = {
 }
 
 async function show(req, res) {
-    // console.log(req.params)
+    console.log(req.params)
     const zone = req.params.zone
     const animals = await Animal.find({ zone: zone }).exec();
-    // console.log(animals)
-    // const animals = await Animal.findById(req.params._id)
+    console.log(`animals: ${animals}`)
     res.render('./animal-zones/zone', { animals, zone })
 }
 
 async function showDetail(req, res) {
     const name = req.params.name
     const animal = await Animal.findOne({ name }).exec();
+
     const shows = await Show.find({ 'animal': animal._id }).exec();
     res.render('./animal-zones/animal', { animal, shows })
 }
