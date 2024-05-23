2449f37b281b2b4b882b582240b3e10d8bcaad8a
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2024-05-23 19:25:43 +1000

    disable booking passed dates in booking edit view

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -21,16 +21,33 @@ async function showBookingEdit(req, res) {
         .findById(req.params.id)
         .populate('animalShow')
         .exec();
-    res.render('bookings/edit', { booking })
+    res.render('bookings/edit', { errorMsg: "", booking })
 }
 
 async function saveBookingChange(req, res) {
-    const booking = await Booking.findById(req.params.id)
-    booking.showDate = req.body.showDate
+    console.log("edit details: ", req.body)
+    const booking = await Booking.findById(req.params.id).populate('animalShow').exec();
+    
     booking.numberOfAdults = req.body.numberOfAdults
     booking.numberOfChildren = req.body.numberOfChildren
-    await booking.save()
-    res.redirect('/bookings')
+    
+    const dateAndTime = req.body.showDate.toString().concat(' ', booking.animalShow.timeOfDay)
+    const dateCombined = new Date(dateAndTime)
+    booking.showDate = dateCombined
+    const today = new Date()
+
+    if(dateCombined.toISOString() < today.toISOString()) {
+        res.render('./bookings/edit', { errorMsg: "Invalid date. Please book for future date.", booking })
+        return
+    }
+
+    try { 
+        await booking.save()
+        res.redirect('/bookings')
+    } catch(err) {
+        console.log(err)
+        res.redirect('/bookings', { errorMsg: err.message })
+    }
 }
 
 async function deleteBooking(req, res) {
